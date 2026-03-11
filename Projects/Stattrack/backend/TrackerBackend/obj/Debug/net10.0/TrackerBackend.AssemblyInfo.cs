9b998fddd62cc9c75a4d42f2b6cd7107ec56c468
William <williamiv.uni@gmail.com>
2026-03-11 16:33:26 -0400

    school

--- Projects/Stattrack/backend/TrackerBackend/obj/Debug/net10.0/TrackerBackend.AssemblyInfo.cs
+++ Projects/Stattrack/backend/TrackerBackend/obj/Debug/net10.0/TrackerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10fa3856f5ef932d26ecda49ea5851f24fa5d2af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d89114d3d629437bc74b574abf011d8bff25292")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
